523e1bbe013e1f0104f2cf81d20e815ff26de41b
jaber saraj <jaber2x@gmail.com>
2024-08-26 19:31:49 +0330

    Finally

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,22 +1,17 @@
 
-
-import React from 'react'
-import { FcNext, FcPrevious, FcDoughnutChart  } from "react-icons/fc";
+import React, { useState } from 'react'
 import SlideShowDetails from './SlideShowDetails'
 
 
 function SlideShow() {
+
   return (
     <>
       <div className='flex h-screen justify-center items-center bg-gray-300'>
         <div className='bg-orange-400 w-3/12 rounded p-6 shadow-lg'>
           <h2 className='flex justify-center pb-3 text-2xl font-semibold mb-3 border-b-2 border-gray-400 text-gray-700'>Comments of colleagues</h2>
           <SlideShowDetails />
-          <div className='flex space-x-32 justify-center pt-16'>
-              <FcPrevious className='cursor-pointer'/>
-              <FcDoughnutChart className='cursor-pointer' />
-              <FcNext className='cursor-pointer'/>
-          </div>
+          
         </div>
       </div>
     </>
--- src/components/SlideShowDetails.jsx
+++ src/components/SlideShowDetails.jsx
@@ -1,35 +1,63 @@
 
 import { data } from './Data'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { FcNext, FcPrevious, FcDoughnutChart } from "react-icons/fc";
 
 
 function SlideShowDetails() {
+
+    const [index, setIndex] = useState(0);
+
+    const nextSlide = () => {
+        if (index > data.length -2) {
+            setIndex(0)
+        }
+        else {
+            setIndex(index + 1)
+        }
+
+    }
+
+    const prevSlide = () => {
+        if (index < 1) {
+            setIndex(2)
+        }
+        else {
+            setIndex(index - 1)
+        }
+
+    }
+
+    const randomNumber=()=>{
+        let rnd=Math.floor(Math.random() * (data.length-1))
+        if(rnd===index){
+            rnd++
+        }
+        setIndex(rnd);
+    }
+
+
     return (
         <>
-            {
-                data.map((item,index) => {
-                    return (
-                        <div key={index}>
-                            <div className='flex justify-center pb-3'>
-                                <img src={item.image} className='rounded-full w-24 h-24'></img>
-                            </div>
-                            <div className='flex justify-center pb-4'>
-                                <h3 className='text-xl font-bold text-gray-800' >{item.name}</h3>
-                            </div>
-                            <div className='flex justify-center pb-4'>
-                                <h4 className='text-lg font-semibold text-gray-700' >{item.job}</h4>
-                            </div>
-                            <div className='flex justify-center'>
-                                <p className='text-justify text-gray-600' >{item.text}</p>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-           
-
-
+            <div>
+                <div className='flex justify-center pb-3'>
+                    <img src={data[index].image} className='rounded-full w-24 h-24'></img>
+                </div>
+                <div className='flex justify-center pb-4'>
+                    <h3 className='text-xl font-bold text-gray-800' >{data[index].name}</h3>
+                </div>
+                <div className='flex justify-center pb-4'>
+                    <h4 className='text-lg font-semibold text-gray-700' >{data[index].job}</h4>
+                </div>
+                <div className='flex justify-center'>
+                    <p className='text-justify text-gray-600' >{data[index].text}</p>
+                </div>
+            </div>
+            <div className='flex space-x-32 justify-center pt-16'>
+                <FcPrevious className='cursor-pointer' onClick={nextSlide} />
+                <FcDoughnutChart className='cursor-pointer' onClick={randomNumber} />
+                <FcNext className='cursor-pointer' onClick={prevSlide} />
+            </div>
         </>
     )
 }
